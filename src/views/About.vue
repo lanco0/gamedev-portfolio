46febdd6de3a5794c48cd2ee8d40ab5a712fed57
Hakkı Tumar <hakkitumar1@gmail.com>
2024-12-20 01:39:07 +0300

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,11 +10,11 @@
 
       <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">stuff</router-link>, on <router-link to="/other-projects">other stuff</router-link>, and took part in <router-link to="/resume">a few things</router-link> as well.</div>
 
-      <div style="margin-top: 40px;">I'm <strong>currently looking for a job</strong> as a monk, like my good friend John Rambo did a few years back. You can reach me at <a href="mailto:johnmatrix@deltaforce.us">johnmatrix@deltaforce.us</a> or <router-link to="/contact">through here</router-link>.</div>
+      <div style="margin-top: 40px;">I'm <strong>currently looking for a job</strong> as a monk, like my good friend John Rambo did a few years back. You can reach me at <a href="mailto:hakkitumar1@gmail.com">hakkitumar1@gmail.com</a> or <router-link to="/contact">through here</router-link>.</div>
     </div>
 
     <div class="photo">
-      <img src="img/77984587.png" alt="Avatar of webrrio" /> 
+      <img src="img/77984587.jpg" alt="Hakki" /> 
     </div>
 
     <div style="clear:both"></div>
